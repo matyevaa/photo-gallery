6950810f0f7a9c818365fd67b1322718479301ea
matyevaa <59642740+matyevaa@users.noreply.github.com>
2021-01-14 05:55:47 +0600

    Form update and css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import './index.css';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
 class UserInput extends React.Component {
-	render() {
+	const [hide, show] = useState(false);
+	const myForm = () => show(true);
 		return (
 
 			<form id="myForm">
 				<h1>Photo Gallery by Alima M.</h1>
 				<input type="text" placeholder="Enter your URL here"/>
+				</br>
 				<input type="text" placeholder="Enter your caption here"/>
-				<button>Accept</button>
+				</br>
+				<button onClick={myForm}>Accept</button>
+				</br>
 				<button>Cancel</button>
 			</form>
 		);
-	}
 }
 
-ReactDOM.render(<UserInput />, document.getElementById('root'));
+const myRoot = document.getElementById('root');
+ReactDOM.render(<UserInput />, myRoot);
