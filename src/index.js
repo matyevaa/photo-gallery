51728c28c3cb0f5c88456d6fb3bd08035dff40ef
matyevaa <59642740+matyevaa@users.noreply.github.com>
2021-01-14 06:42:29 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,18 @@ import './index.css';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const {
-	revealForm
-} = this.state;
 
 class UserInput extends React.Component {
+
 	return (
-		{
-			<p onClick={() => this.setState({revealForm: !revealForm})}>Click Here</p>
-			{ revealForm
-				?
-				<form id="myForm">
-					<h1>Photo Gallery by Alima M.</h1>
-					<input type="text" placeholder="Enter your URL here"/></br>
-					<input type="text" placeholder="Enter your caption here"/></br>
-					<button>Accept</button></br>
-					<button>Cancel</button>
-				</form>
-				: 
-				null
-			}
-		}
+			<p>Click Me</p>
+			<form id="myForm">
+				<h1>Photo Gallery by Alima M.<\h1>
+				<input type="text" placeholder="Enter your URL here"/><br>
+				<input type="text" placeholder="Enter your caption here"/><br>
+				<button>Accept</button><br>
+				<button>Cancel</button>
+			</form>
 	);
 }
 
