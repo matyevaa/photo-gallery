61a79a02fb0c66bef18398c2e656ccf2c0ee4cf0
matyevaa <59642740+matyevaa@users.noreply.github.com>
2021-01-14 06:26:55 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@ import ReactDOM from 'react-dom';
 
 
 class UserInput extends React.Component {
-	const [show, hide] = useState(false);
-	const myForm = () => show(true);
-		return (
+	
+	const {
+		revealForm
+	} = this.state;
 
+	return (
+		<p onClick={() => this.setState({revealForm: !revealForm})}>Click Here</p>
+		{ revealForm
+			?
 			<form id="myForm">
 				<h1>Photo Gallery by Alima M.</h1>
-				<input type="text" placeholder="Enter your URL here"/>
-				</br>
-				<input type="text" placeholder="Enter your caption here"/>
-				</br>
-				<button onClick={myForm}>Accept</button>
-				</br>
+				<input type="text" placeholder="Enter your URL here"/></br>
+				<input type="text" placeholder="Enter your caption here"/></br>
+				<button>Accept</button></br>
 				<button>Cancel</button>
 			</form>
-		);
+			: 
+			null
+		}
+	);
 }
 
 const myRoot = document.getElementById('root');
-ReactDOM.render(<UserInput />, myRoot);
+ReactDOM.render(
+	<UserInput />, 
+	myRoot
+);
